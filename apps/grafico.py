e12f95bac109849bafbe344ade578989698b968b
Pedro Victor <ppvitorr@gmail.com>
2020-06-17 19:13:33 -0300

    update grafico.py add some parent divs for styling

--- apps/grafico.py
+++ apps/grafico.py
@@ -3,6 +3,7 @@ import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 from datetime import datetime as dt
 from datetime import date
@@ -22,13 +23,14 @@ df['data'] = pd.to_datetime(df['data'])
 current_year = 2020
 
 fig = px.choropleth(df,
-    geojson=geojson_uf,
-    locations='estado',
-    color='anomalo',
-    featureidkey='properties.UF_05',
-    scope='south america',
-)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, clickmode='event+select')
+                    geojson=geojson_uf,
+                    locations='estado',
+                    color='anomalo',
+                    featureidkey='properties.UF_05',
+                    scope='south america'
+                    )
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0,
+                        "b": 0}, clickmode='event+select')
 
 previous_click = None
 
@@ -37,16 +39,20 @@ layout = html.Div(
         html.H1(
             id='dados-estado',
             children='Dados',
-            style={'textAlign': 'center'}
+            style={'textAlign': 'center',
+                    'color': 'rgba(255,255,255,0.90)'}
         ),
 
         html.Div(
             'Compras de Respiradores',
-            style={'textAlign': 'center'}
+            style={'textAlign': 'center',
+                    'marginTop': '1%',
+                    'color': 'rgba(255,255,255,0.90)'}
         ),
 
         html.Div(
             id="map_geo_outer",
+            className="map-container",
             children=[
                 dcc.Graph(
                     id="choropleth",
@@ -56,53 +62,64 @@ layout = html.Div(
         ),
 
         html.Div(
-            className="row",
-            style={'marginTop': 30, 'marginBottom': 0},
+            className='selectors-parent',
             children=[
-                dcc.DatePickerRange(
-                    id='date-picker',
-                    display_format='D/M/Y',
-                    min_date_allowed=df['data'].min(),
-                    max_date_allowed=df['data'].max(),
-                    initial_visible_month=dt(current_year,df['data'].max().month, 1),
-                    start_date=df['data'].min(),
-                    end_date=df['data'].max(),
+                html.Div(
+                    className="date-selector",
+                    children=[
+                        dcc.DatePickerRange(
+                            id='date-picker',
+                            display_format='D/M/Y',
+                            min_date_allowed=df['data'].min(),
+                            max_date_allowed=df['data'].max(),
+                            initial_visible_month=dt(
+                                current_year, df['data'].max().month, 1),
+                            start_date=df['data'].min(),
+                            end_date=df['data'].max(),
+                        ),
+                    ],
                 ),
-            ],
-        ),
+                html.Div(
+                    className='price-slider-parent',
+                    children=[
+                        html.Span(
+                            'Faixa de Preço: R$ %s - R$ %s' % (
+                                df['preco'].min(), df['preco'].max()),
+                            id='display-price-slider',
+                            className="price-span",
+                        ),
+                        dcc.RangeSlider(
+                            id='price-slider',
+                            min=df['preco'].min(),
+                            max=df['preco'].max(),
+                            step=1,
+                            value=[df['preco'].min(), df['preco'].max()],
+                        )
+                    ]
+                ),
 
-        html.Div(
-            style={'marginTop': 15},
-            children=[
-                html.Span(
-                    'Faixa de Preço: %sR$ - %sR$' % (df['preco'].min(), df['preco'].max()),
-                    id='display-price-slider'
+                html.Div(
+                    className='amount-slider-parent',
+                    children=[
+                        html.Span(
+                            'Quantidade: %s - %s' % (df['quantidade'].min(),
+                                                    df['quantidade'].max()),
+                            id='display-amount-slider'
+                        ),
+                        dcc.RangeSlider(
+                            id='amount-slider',
+                            min=df['quantidade'].min(),
+                            max=df['quantidade'].max(),
+                            step=1,
+                            value=[df['quantidade'].min(
+                            ), df['quantidade'].max()],
+                        )
+                    ]
                 ),
-                dcc.RangeSlider(
-                    id='price-slider',
-                    min=df['preco'].min(),
-                    max=df['preco'].max(),
-                    step=1,
-                    value=[df['preco'].min(), df['preco'].max()],
-                )
             ]
         ),
 
-        html.Div(
-            children=[
-                html.Span(
-                    'Quantidade: %s - %s' % (df['quantidade'].min(), df['quantidade'].max()),
-                    id='display-amount-slider'
-                ),
-                dcc.RangeSlider(
-                    id='amount-slider',
-                    min=df['quantidade'].min(),
-                    max=df['quantidade'].max(),
-                    step=1,
-                    value=[df['quantidade'].min(), df['quantidade'].max()],
-                )
-            ]
-        ),
+
 
         html.Div(
             style={'textAlign': 'center'},
@@ -110,11 +127,11 @@ layout = html.Div(
                 dash_table.DataTable(
                     id='table',
                     columns=[
-                        {'name':'Data', 'id':'data'},
-                        {'name':'Estado', 'id':'estado'},
-                        {'name':'Compra', 'id':'nome'},
-                        {'name':'Preço', 'id':'preco'},
-                        {'name':'Quantidade', 'id':'quantidade'},
+                        {'name': 'Data', 'id': 'data'},
+                        {'name': 'Estado', 'id': 'estado'},
+                        {'name': 'Compra', 'id': 'nome'},
+                        {'name': 'Preço', 'id': 'preco'},
+                        {'name': 'Quantidade', 'id': 'quantidade'},
                     ],
                     column_selectable='multi',
                     data=df.assign(
@@ -163,11 +180,11 @@ layout = html.Div(
                     ],
                     style_data_conditional=[
                         {
-                            'if':{'column_id': 'nome'},
+                            'if': {'column_id': 'nome'},
                             'textAlign': 'left',
                         },
                         {
-                            'if':{'filter_query': '{{anomalo}} = {}'.format(-1),},
+                            'if': {'filter_query': '{{anomalo}} = {}'.format(-1), },
                             'backgroundColor': '#fff3cd',
                         },
                     ],
@@ -180,7 +197,7 @@ layout = html.Div(
                 )
             ]
         )
-])
+    ])
 
 
 def update_datatable(start_date, end_date, price_limit, amount_limit, state_click):
@@ -198,7 +215,6 @@ def update_datatable(start_date, end_date, price_limit, amount_limit, state_clic
         & (updated_df['data'] <= end_date)
     ]
 
-
     updated_df = updated_df[
         (updated_df['preco'] >= price_limit[0])
         & (updated_df['preco'] <= price_limit[1])
@@ -214,6 +230,7 @@ def update_datatable(start_date, end_date, price_limit, amount_limit, state_clic
     ).to_dict("rows")
     return updated_df
 
+
 @app.callback(
     Output('table', 'data'),
     [
@@ -225,8 +242,10 @@ def update_datatable(start_date, end_date, price_limit, amount_limit, state_clic
     ],
 )
 def update_data(start_date, end_date, price_limit, amount_limit, state_click):
-    data = update_datatable(start_date[:10], end_date[:10], price_limit, amount_limit, state_click)
+    data = update_datatable(
+        start_date[:10], end_date[:10], price_limit, amount_limit, state_click)
     return data
+
 
 @app.callback(
     Output('display-price-slider', 'children'),
@@ -234,6 +253,7 @@ def update_data(start_date, end_date, price_limit, amount_limit, state_click):
 )
 def update_price_slider(limits):
     return 'Faixa de Preço: %sR$ - %sR$' % (limits[0], limits[1])
+
 
 @app.callback(
     Output('display-amount-slider', 'children'),
