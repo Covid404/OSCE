a1e0c8852ea3fdf55b9405479fd83977e79f236c
Alberto Sobrinho <aclsobrinho@hotmail.com>
2020-06-18 14:14:57 -0300

    Tirando css externos

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import dash
 
-external_stylesheets = [
-    'https://codepen.io/chriddyp/pen/bWLwgP.css',
-]
-
 app = dash.Dash('Covis - Dashboard', suppress_callback_exceptions=True)
 server = app.server
