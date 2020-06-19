c50742b319dae9838cb4f214f9200fb90076f5b3
Alberto Sobrinho <aclsobrinho@hotmail.com>
2020-06-18 21:57:03 -0300

    adicionando fontes nos dados

--- apps/grafico.py
+++ apps/grafico.py
@@ -24,16 +24,16 @@ current_year = 2020
 
 map_df = pd.read_csv('data/predictions.csv')
 map_df = map_df[map_df['anomalo'] != 1]
-map_df = map_df[['estado', 'anomalo']].groupby('estado', as_index=False).count()
+map_df = map_df[['estado', 'anomalo']].groupby('estado', as_index=False).mean()
 
 fig = px.choropleth(map_df,
-                    geojson=geojson_uf,
-                    locations='estado',
-                    color='anomalo',
-                    featureidkey='properties.UF_05',
-                    scope='south america',
-                    color_continuous_scale='ylorrd'
-                    )
+    geojson=geojson_uf,
+    locations='estado',
+    color='anomalo',
+    featureidkey='properties.UF_05',
+    scope='south america',
+    color_continuous_scale='ylorrd_r'
+)
 fig.update_layout(margin={"r": 0, "t": 0, "l": 0,
                         "b": 0}, clickmode='event+select')
 
@@ -47,8 +47,10 @@ def update_dataframe(df, start_date, end_date, price_limit, amount_limit, states
     start_date = dt.strptime(start_date, '%Y-%m-%d')
     end_date = dt.strptime(end_date, '%Y-%m-%d')
     updated_df = updated_df[
-        (updated_df['data'] >= start_date)
-        & (updated_df['data'] <= end_date)
+        (
+            (updated_df['data'] >= start_date)
+            & (updated_df['data'] <= end_date)
+        ) | (updated_df['data'].isnull())
     ]
 
     updated_df = updated_df[
@@ -75,25 +77,31 @@ def update_dataframe(df, start_date, end_date, price_limit, amount_limit, states
     return updated_df
 
 def create_row(row):
-    if row['anomalo'] == -1:
-        return html.Tr(
-            [html.Td(value, style={'fontSize': 'small'}) for index, value in row.iteritems() if index != 'anomalo'],
-            className='suspeito'
-        )
-    return html.Tr(
-        [html.Td(value, style={'fontSize': 'small'}) for index, value in row.iteritems() if index != 'anomalo'],
-    )
+    tr_children = []
+    for index, value in row.iteritems():
+        if index not in ['anomalo', 'Fonte']:
+            if index == 'Compra':
+                style = {'fontSize': 'small', 'textAlign': 'center'}
+                td_children = [value, ' (', html.A('Fonte', href=row['Fonte']), ')']
+            else:
+                style = {'fontSize': 'small', 'textAlign': 'center'}
+                td_children = [value]
+            tr_children.append(html.Td(style=style, children=td_children))
+
+    if row['anomalo'] < 5:
+        return html.Tr(className='suspeito', children=tr_children)
+    return html.Tr(children=tr_children)
 
 def update_table(df):
     return html.Table(
-        className='table table-sm table-bordered table-hover',
+        className='table table-sm table-hover',
         style={'backgroundColor': 'white'},
         children=[
             html.Thead(
                 html.Tr(
                     [
-                        html.Th(column, style={'fontSize': 'small'})
-                        for column in df.columns if column != 'anomalo'
+                        html.Th(column, style={'fontSize': 'small', 'textAlign': 'center'})
+                        for column in df.columns if column not in ['anomalo', 'Fonte']
                     ]
                 )
             ),
@@ -111,15 +119,6 @@ layout = html.Div(
             style={'textAlign': 'center',
                     'color': colors['text']}
         ),
-
-        # html.Div(
-        #     children=[
-        #         dcc.Graph(
-        #             id="choropleth",
-        #             figure=fig,
-        #         ),
-        #     ],
-        # ),
 
         html.Div(
             className='row row-cols-1 row-cols-md-3 mt-1',
@@ -184,7 +183,7 @@ layout = html.Div(
             className='row',
             children=[
                 html.Div(
-                    className='col p-0',
+                    className='col-12 p-0',
                     children=dcc.Graph(
                         id="choropleth",
                         figure=fig,
@@ -192,7 +191,7 @@ layout = html.Div(
                     style={'maxWidth': '30%'}
                 ),
                 html.Div(
-                    className='col p-0',
+                    className='col-12 p-0',
                     children=html.Div(
                         id='data-table',
                         className='table-responsive',
@@ -201,127 +200,8 @@ layout = html.Div(
                 ),
             ]
         ),
-
-        # html.Div(
-        #     id='data-table',
-        #     className='table-responsive',
-        #     style={'maxWidth': '400px'}
-        # ),
-
-        # html.Div(
-        #     style={'textAlign': 'center'},
-        #     className="table-parent",
-        #     children=[
-        #         dash_table.DataTable(
-        #             id='table',
-        #             columns=[
-        #                 {'name': 'Data', 'id': 'data'},
-        #                 {'name': 'UF', 'id': 'estado'},
-        #                 {'name': 'Compra', 'id': 'nome'},
-        #                 {'name': 'Preço', 'id': 'preco'},
-        #                 {'name': 'Quantidade', 'id': 'quantidade'},
-        #             ],
-        #             column_selectable='multi',
-        #             data=df.assign(
-        #                 **df.select_dtypes(['datetime']).astype(str).to_dict('list')
-        #             ).to_dict('records'),
-        #             sort_action='native',
-        #             style_header={
-        #                 'backgroundColor': colors['text'],
-        #                 'fontWeight': 'bold',
-        #                 'fontSize' : '18px',
-        #             },
-        #             style_cell={'textAlign': 'center'},
-        #             style_cell_conditional=[
-        #                 {
-        #                     'if': {'column_id': 'nome'},
-        #                     'maxWidth': '400px',
-        #                     'overflow': 'hidden',
-        #                     'textOverflow': 'ellipsis',
-        #                 },
-        #             ],
-        #             style_data_conditional=[
-        #                 {
-        #                     'if': {'column_id': 'nome'},
-        #                     'textAlign': 'left',
-        #                 },
-        #                 {
-        #                     'if': {'row_index' : 'odd'},
-        #                     'backgroundColor' : 'rgb(248, 248, 248)'
-        #                 },
-        #                 {
-        #                     'if': {'row_index' : 'even'},
-        #                     'backgroundColor' : 'rgb(230, 230, 230)'
-        #                 },
-        #                 {
-        #                     'if': {'filter_query': '{{anomalo}} = {}'.format(-1),
-        #                             'row_index' : 'odd'},
-        #                     'backgroundColor' : 'rgb(255, 205, 208)'
-        #                 },
-        #                 {
-        #                     'if': {'filter_query': '{{anomalo}} = {}'.format(-1),
-        #                             'row_index' : 'even'},
-        #                     'backgroundColor' : '#ffbabf'
-        #                 },
-        #             ],
-        #             tooltip_data=[
-        #                 {
-        #                     'nome': {'value': row['nome'], 'type': 'markdown'}
-        #                 } for row in df.to_dict('rows')
-        #             ],
-        #             tooltip_duration=None,
-        #         )
-        #     ]
-        # )
-    ])
-
-
-def update_datatable(start_date, end_date, price_limit, amount_limit, state_click):
-
-    if state_click is not None:
-        states = [point['location'] for point in state_click['points']]
-        updated_df = df[df['estado'].isin(states)]
-    else:
-        updated_df = df
-
-    start_date = dt.strptime(start_date, '%Y-%m-%d')
-    end_date = dt.strptime(end_date, '%Y-%m-%d')
-    updated_df = updated_df[
-        (updated_df['data'] >= start_date)
-        & (updated_df['data'] <= end_date)
     ]
-
-    updated_df = updated_df[
-        (updated_df['preco'] >= price_limit[0])
-        & (updated_df['preco'] <= price_limit[1])
-    ]
-
-    updated_df = updated_df[
-        (updated_df['quantidade'] >= amount_limit[0])
-        & (updated_df['quantidade'] <= amount_limit[1])
-    ]
-
-    updated_df = updated_df.assign(
-        **updated_df.select_dtypes(['datetime']).astype(str).to_dict('list')
-    ).to_dict("rows")
-    return updated_df
-
-
-@app.callback(
-    Output('table', 'data'),
-    [
-        Input('date-picker', 'start_date'),
-        Input('date-picker', 'end_date'),
-        Input('price-slider', 'value'),
-        Input('amount-slider', 'value'),
-        Input('choropleth', 'selectedData')
-    ],
 )
-def update_data(start_date, end_date, price_limit, amount_limit, state_click):
-    data = update_datatable(
-        start_date[:10], end_date[:10], price_limit, amount_limit, state_click)
-    return data
-
 
 @app.callback(
     Output('data-table', 'children'),
