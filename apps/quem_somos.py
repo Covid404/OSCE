30e1ae7ec7699989c8f0fa6d50eae9b2ce5106f9
Alberto Sobrinho <aclsobrinho@hotmail.com>
2020-06-20 16:54:58 -0300

    typo

--- apps/quem_somos.py
+++ apps/quem_somos.py
@@ -21,7 +21,7 @@ layout = html.Div([
             }
         ),
         html.Ul(children=[html.Li(children=f'Líder: Asian Shay (https://github.com/aianshay)'),
-            html.Li(children='Web Dev: Alberto Costa (https://github.com/a-skz)'),
+            html.Li(children='Web Dev: Alberto Sobrinho (https://github.com/a-skz)'),
             html.Li(children='Web Dev: Pedro Arouck (https://github.com/Arouck)'),
             html.Li(children='Data Analyst: Renan Cunha (https://github.com/renan-cunha)'),
             html.Li(children='Data Analyst: Renato Mota (https://github.com/Rjlmota)')],
